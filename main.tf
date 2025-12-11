1f55be8a95a6655673d548bf64a41a6897a67723
Joshua Geissler <jgeissler14@gmail.com>
2025-12-11 14:24:58 -0800

    feat: overhaul to ec2

--- main.tf
+++ main.tf
@@ -16,65 +16,84 @@ module "storage_bucket" {
   context = module.this.context
 }
 
-# ------------------------------------------------------------------------------
-# Autoscaling Group Example
-#
-# This is an example of how to use the cloudposse/label/null module directly
-# to generate a consistent name and tags for related AWS resources.
-# ------------------------------------------------------------------------------
+module "vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "2.1.0"
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-module "asg_label" {
-  source = "cloudposse/label/null"
+  ipv4_primary_cidr_block = "172.16.0.0/16"
 
   context = module.this.context
 }
 
-resource "aws_launch_configuration" "example" {
-  # The `id` output from the label module provides the standardized resource name prefix.
-  name_prefix   = module.asg_label.id
-  image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.4.2"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  availability_zones   = var.availability_zones
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
+
+  context = module.this.context
 }
 
-resource "aws_autoscaling_group" "example" {
-  # The `id` output from the label module provides the standardized resource name.
-  name = module.asg_label.id
-  min_size                  = 0
-  max_size                  = 1
-  desired_capacity          = 0 # Set to 0 to avoid running instances by default
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-  launch_configuration      = aws_launch_configuration.example.id
-  vpc_zone_identifier       = [] # NOTE: This needs to be populated with subnet IDs for a real deployment
+module "instance_profile_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
 
-  # The `tags` output provides the full map of standardized tags.
-  dynamic "tag" {
-    for_each = module.this.tags_as_list_of_maps
-    content {
-      key                 = tag.value.key
-      propagate_at_launch = tag.value.propagate_at_launch
-      value               = tag.value.value
+  attributes = distinct(compact(concat(module.this.attributes, ["profile"])))
+
+  context = module.this.context
+}
+
+data "aws_iam_policy_document" "test" {
+  count = module.this.enabled ? 1 : 0
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
     }
   }
 }
 
+resource "aws_iam_role" "test" {
+  count = module.this.enabled ? 1 : 0
+
+  name               = module.instance_profile_label.id
+  assume_role_policy = one(data.aws_iam_policy_document.test[*].json)
+  tags               = module.instance_profile_label.tags
+}
+
+# https://github.com/hashicorp/terraform-guides/tree/master/infrastructure-as-code/terraform-0.13-examples/module-depends-on
+resource "aws_iam_instance_profile" "test" {
+  count = module.this.enabled ? 1 : 0
+
+  name = module.instance_profile_label.id
+  role = aws_iam_role.test[0].name
+}
+
+module "ec2_instance" {
+  source = "cloudposse/ec2-instance/aws"
+
+  vpc_id                      = module.vpc.vpc_id
+  subnet                      = module.this.enabled ? module.subnets.private_subnet_ids[0] : null
+  security_groups             = [module.vpc.vpc_default_security_group_id]
+  assign_eip_address          = var.assign_eip_address
+  associate_public_ip_address = var.associate_public_ip_address
+  instance_type               = var.instance_type
+  security_group_rules        = var.security_group_rules
+  instance_profile            = module.this.enabled ? aws_iam_instance_profile.test[0].name : null
+  tenancy                     = var.tenancy
+
+  depends_on = [aws_iam_instance_profile.test]
+
+  context = module.this.context
+}
--- tfvars/prod.tfvars
+++ tfvars/prod.tfvars
@@ -1,13 +1,50 @@
+# enabled = true
+
+region = "us-east-1"
+
 namespace = "es"
-stage     = "prod"
+
+stage = "prod"
+
 name      = "app"
+
 tenant    = "internal"
 
-tags = {
-  BusinessUnit = "finance"
-  ManagedBy    = "terraform"
-}
+availability_zones = ["us-east-1a", "us-east-1b"]
 
-additional_tag_map = {
-  propagate_at_launch = "true"
-}
+assign_eip_address = false
+
+associate_public_ip_address = true
+
+instance_type = "t3.micro"
+
+security_group_rules = [
+  {
+    type        = "egress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    type        = "ingress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    type        = "ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    type        = "ingress"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+]
